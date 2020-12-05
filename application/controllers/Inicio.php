ed4fe28645243a079e2344e8b58333185c019a15
Edwin Pulido <edwin@Edwins-MacBook-Pro.local>
2020-12-04 19:44:39 -0600

    Portada inicio terminada

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -4,8 +4,12 @@ class Inicio extends Public_Controller {
 
     public function index() {
         
+        $this->load->model('noticias_model');
+
+        $data['noticias'] = $this->noticias_model->getNoticias();
+
         $this->template->title('Yale - School of Art');
-        $this->template->build('inicio/portada.php');
+        $this->template->build('inicio/portada.php', $data);
     }
 
 }
--- application/views/inicio/portada.php
+++ application/views/inicio/portada.php
@@ -1,9 +1,9 @@
 <section>
     <div class = "slider avisos">
-        <div class = "just_text aviso" style = "background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('<?=base_url()?>assets/img/escultura.jpg')">
+        <div class = "just_text aviso" style = "background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('<?=base_url()?>assets/img/avisos/escultura.jpg')">
             <div class = "texto-aviso">Este es un nuevo aviso de escultura</div>
         </div>
-        <div class = "just_text aviso" style = "background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('<?=base_url()?>assets/img/pintura.jpg')">
+        <div class = "just_text aviso" style = "background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('<?=base_url()?>assets/img/avisos/pintura.jpg')">
             <div class = "texto-aviso">Este es un nuevo aviso de pintura</div>
         </div>
     </div>
@@ -11,27 +11,17 @@
     <div class = "noticias">
         <h3>Noticias recientes</h3>
         <div class = "row row-noticias">
-            <div class = "col noticia-principal" style = "background-image: url('<?=base_url()?>assets/img/pintura.jpg')">
-                <div class = "titulo-noticia-principal">Edwin</div>
+            <?php foreach($noticias as $noticia): ?>
+            <div class = "col-md-3 box-noticia-reciente">
+                <a href = "<?=base_url()?>noticias/<?=$noticia->id_noticia?>/<?=$noticia->slug?>">
+                    <div class = "noticia-reciente" style = "background-image: url(<?=base_url()?>assets/img/noticias/<?=$noticia->imagen?>)">
+                        <div class = "noticia-reciente-content">
+                            <?= $noticia->titulo ?>
+                        </div>
+                    </div>
+                </a>
             </div>
-            <div class = "col noticia-normal" style = "padding: 0;">
-                <div class = "row row-noticias">
-                    <div class = "col noticia">
-                        <div class = "titulo-noticia">Edwin</div>
-                    </div>
-                    <div class = "col noticia">
-                        <div class = "titulo-noticia">Edwin</div>
-                    </div>
-                </div>
-                <div class = "row row-noticias">
-                    <div class = "col noticia">
-                        <div class = "titulo-noticia">Edwin</div>
-                    </div>
-                    <div class = "col noticia">
-                        <div class = "titulo-noticia">Edwin</div>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </section>
